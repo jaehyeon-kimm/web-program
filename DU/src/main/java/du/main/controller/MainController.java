8767f2d65fca944913fe0eb303d96ed3d2b8e6b9
Jae Hyeon Kim <46950724+jaehyeon-king@users.noreply.github.com>
2021-08-10 13:52:49 +0900

    add DU

---
+++ DU/src/main/java/du/main/controller/MainController.java
@@ -0,0 +1,19 @@
+package du.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value="/main.do", method = RequestMethod.POST)
+	public String mainPage() {
+		return "main.html";
+	}
+	
+	@RequestMapping(value="/login.do", method = RequestMethod.GET)
+	public String loginPage() {
+		return "login.html";
+	}
+}
